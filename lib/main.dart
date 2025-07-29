cc459e40917774d9ae9c7db23b2f41cc2e5c3845
kboy101222 <george.kyle87@gmail.com>
2025-07-29 16:37:31 -0500

    I'm actually an idiot

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ void main() {
   // [Document] renders the root document structure (<html>, <head> and <body>)
   // with the provided parameters and components.
   runApp(Document(
-    // base: 'Personal-Website-Remade',
+    base: 'Personal-Website-Remade',
     title: 'personal_site_remade',
     styles: [
       // Special import rule to include to another css file.
